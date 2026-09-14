e244a457f539dac7e6df220b46ad9b592bc3d649
ilabyubuan67 <buan.rg@obmontessori.edu.ph>
2026-09-15 07:41:35 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from pyscript import display
+#integer
+
+# 0
+
+a = 25 #int
+b = 88.99 
+display(float(a))
+display(int(b))
+
+
+num1 = 65
+num2 = "65"
+num3 = 65.0
+
+display(int(num1))
+display(int(num2))
+display(float(num3))
+
+display(num1 + int(num2))
+display(num3 + float(num1))
+display(float(num2) + num3)
+
+
