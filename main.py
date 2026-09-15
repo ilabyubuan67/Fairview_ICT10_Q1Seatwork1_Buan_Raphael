17c55a884f1382f81a5743ee1b9dd8b5fb68bd0c
ilabyubuan67 <buan.rg@obmontessori.edu.ph>
2026-09-15 08:58:29 +0800

    SW

--- main.py
+++ main.py
@@ -1,24 +1,60 @@
-from pyscript import display
-#integer
-
-# 0
-
-a = 25 #int
-b = 88.99 
-display(float(a))
-display(int(b))
+from pyscript import document, display
 
 
-num1 = 65
-num2 = "65"
-num3 = 65.0
+# 
 
-display(int(num1))
-display(int(num2))
-display(float(num3))
+a = "Student Name: Raphael Buan"
+b = "Age: 15"
+height1 = "Height: 165cm"
+_Countries3 = "Countries I want to visit: Canada, South Korea, America"
+student_type = "Are you a student?: True"
+#Dict
+_things3 = {"Shoe Brand: Nike", "Car Brand: Tesla", "Best Friend: Dustin"}
+#Set
+fruit5 = ({"Apple": "Grapes", "Oranges": "Banana", "Mangoes": "Pineapple"})
+#tuple
+_Sevendays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
-display(num1 + int(num2))
-display(num3 + float(num1))
-display(float(num2) + num3)
+
+display((a))
+display((b))
+display((height1))
+display((student_type))
+display(( _Countries3))
+display(( _things3))
+display((fruit5))
+display((_Sevendays))
+
+def Add(g):
+    document.getElementById("result").innerHTML = ""
+    a = int(document.getElementById("input1").value)
+    b = int(document.getElementById("input2").value)
+    result1 = a + b
+
+    display(result1, target="result")
+
+    def Subtraction(g):
+    document.getElementById("result").innerHTML = ""
+    c = int(document.getElementById("input3").value)
+    d = int(document.getElementById("input4").value)
+    result1 = c - d
+
+    display(result1, target="result")
+
+    def Multiply(g):
+    document.getElementById("result").innerHTML = ""
+    e = int(document.getElementById("input5").value)
+    f = int(document.getElementById("input6").value)
+    result1 = e * f
+
+    display(result1, target="result")
+
+    def Division(g):
+    document.getElementById("result").innerHTML = ""
+    a = int(document.getElementById("input7").value)
+    b = int(document.getElementById("input8").value)
+    result1 = a / b
+    display(result1, target="result")
+
 
 
